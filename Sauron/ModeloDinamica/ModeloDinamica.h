9fcbd270fcdc388073f5ae67c3e1027cbad70b29
budsbd <budsbd@8373e73c-ebb0-11dd-9ba5-89a75009fd5d>
2009-10-30 16:49:17 +0000

    Atualizações no modelo de dinâmica

--- Sauron/ModeloDinamica/ModeloDinamica.h
+++ Sauron/ModeloDinamica/ModeloDinamica.h
@@ -10,60 +10,48 @@ namespace sauron
 
 	namespace modeloDinamica
 	{
-		
-		class ModeloDinamica : IDynamicModel
+
+		class ModeloDinamica : public IDynamicModel
 		{
-			private:
-				
-				
-				
-				MedidaOdometro medidaOdometro;
-				
-				Pose posicaoEstimada;
-				
-				pose_t calculaX();
-				pose_t calculaY();
-				pose_t calculaTheta();
-
-				double getVarianciaLinear();
-				double getVarianciaAngular();
-				
-				double influLinearLinear;
-				double influLinearAngular;
-				double influAngularLinear;
-				double influAngularAngular;
-
-				void init()
-				{
-					influLinearLinear = 0.033;
-					influLinearAngular = 0.001;
-					influAngularLinear = 0;
-					influAngularAngular = 0.035;
-				}
-
-			    void atualizaCovariancia(Covariance &dynNoise);
-
-				void atualizaModel(Model &dynModel);
-
-				void atualizaFValue(Matrix &fValue);
 
 
-			public:
-				ModeloDinamica(ArRobot& robot);
-				ModeloDinamica(Pose posicaoEstimada, ArRobot& robot);
-				
-				
-				
-				void updateModel( const Pose &last, 
-                                  Matrix &fValue, Model &dynModel, Covariance &dynNoise );
+		public:
+			ModeloDinamica(ArRobot& robot);
+			ModeloDinamica(Pose posicaoEstimada, ArRobot& robot);
+			void updateModel( const Pose &last, 
+				Matrix &fValue, Model &dynModel, Covariance &dynNoise );
 
+		private:
 
-				
+			MedidaOdometro medidaOdometro;
 
+			Pose posicaoEstimada;
+
+			pose_t calculaX();
+			pose_t calculaY();
+			pose_t calculaTheta();
+
+			double getVarianciaLinear();
+			double getVarianciaAngular();
+
+			double influLinearLinear;
+			double influLinearAngular;
+			double influAngularLinear;
+			double influAngularAngular;
+
+			void init()
+			{
+				influLinearLinear = 0.033;
+				influLinearAngular = 0.001;
+				influAngularLinear = 0;
+				influAngularAngular = 0.035;
+			}
+
+			void atualizaCovariancia(Covariance &dynNoise);
+
+			void atualizaModel(Model &dynModel);
+
+			void atualizaFValue(Matrix &fValue);			
 		};
-
-
-
 	}
-
 }
