dcd07b52711f0845f582d14afbe5809514c429d0
budsbd <budsbd@8373e73c-ebb0-11dd-9ba5-89a75009fd5d>
2009-10-30 14:46:42 +0000

    LocalizationManager incompleto

--- trunk/Sauron/Localization/LocalizationManager.cpp
+++ trunk/Sauron/Localization/LocalizationManager.cpp
@@ -1,16 +1,26 @@
+#include "Aria.h"
 #include "LocalizationManager.h"
 #include "ExtendedKalmanFilter.h"
 #include "OdometerBasedDynamic.h"
 #include "SensorSonar.h"
-#include "Sonar/SonarModel.h"
-#include "Sonar/Configs.h"
+#include "Sonar/PhysicalSonars.h"
 
 namespace sauron
 {
-	LocalizationManager::LocalizationManager(const Map& map)
-		: m_map(map), mp_dynamic(buildDefaultDynamic()), mp_ekf(buildDefaultEKF())
+	LocalizationManager::LocalizationManager(ArRobot* robot, const Map& map)
+		: mp_robot(robot),  m_map(map), mp_dynamic(buildDefaultDynamic()), 
+		mp_ekf(buildDefaultEKF()), mp_sonarDataProvider(buildDefaultSonarDataProvider())
 	{
 		buildDefaultSensors();
+	}
+
+	LocalizationManager::LocalizationManager(ArRobot* robot, const Map& map,
+		const Pose& initialPose)
+		: mp_robot(robot),  m_map(map), mp_dynamic(buildDefaultDynamic()), 
+		mp_ekf(buildDefaultEKF()), mp_sonarDataProvider(buildDefaultSonarDataProvider())
+	{
+		buildDefaultSensors();
+		setInitialPose(initialPose);
 	}
 
 	IDynamicModel* LocalizationManager::buildDefaultDynamic()
@@ -23,6 +33,11 @@ namespace sauron
 		return new ExtendedKalmanFilter();
 	}
 
+	ISonarDataAsyncProvider* LocalizationManager::buildDefaultSonarDataProvider()
+	{
+		return new PhysicalSonars(mp_robot);
+	}
+
 	void LocalizationManager::buildDefaultSensors()
 	{
 		buildDefaultSonars();
@@ -32,7 +47,30 @@ namespace sauron
 	void LocalizationManager::buildDefaultSonars()
 	{
 		for(int i = 0; i < 8; i++) {
-			m_sonarModels.push_back(ISonarModelPtr(new SonarModel(configs::sonars::getSonarPose(i))));
+			m_sensors.push_back(ISensorModelPtr(new SensorSonar(i, *this, *mp_sonarDataProvider)));
+		}
+	}
+
+	void LocalizationManager::buildDefaultVision()
+	{
+		// TODO
+	}
+
+	void LocalizationManager::mainLoop()
+	{
+		while(localize) {
+			// predict
+			Matrix fValue; Model dynModel;	Covariance dynNoise;
+			mp_dynamic->updateModel(this->getPose(), fValue, dynModel, dynNoise);
+			mp_ekf->predict(fValue, dynModel, dynNoise);
+			// update
+			for(std::vector<ISensorModelPtr>::iterator it = m_sensors.begin();
+				it != m_sensors.end(); it++) {
+					Matrix hValue; Measure z; Model H; Covariance R;
+					if((*it)->getEstimate(this->getPose(), hValue, z, H, R)) {
+						mp_ekf->update(z, hValue, H, R);
+					}
+			}
 		}
 	}
 
