ff20856a421bdf74d19249794b1f675d35301f04
budsbd <budsbd@8373e73c-ebb0-11dd-9ba5-89a75009fd5d>
2009-11-06 20:34:06 +0000

    LOG + Erros corrigidos do EKF e SensorSonar

--- Sauron/Localization/SensorSonar.cpp
+++ Sauron/Localization/SensorSonar.cpp
@@ -1,5 +1,7 @@
 #include "SensorSonar.h"
 
+#include "log.h"
+#define SONAR_LOG(level) FILE_LOG(level) << "SensorSonar #"<< m_sonarNumber << ": "
 #include "ILocalizationManager.h"
 #include "MathHelper.h"
 #include "Sonar/Configs.h"
@@ -15,6 +17,7 @@ namespace sauron
 		m_localization(localizationManager), m_callback(this, &SensorSonar::addReadingToModel),
 		m_model(configs::sonars::getSonarPose(sonarNumber))
 	{
+		SONAR_LOG(logINFO) << " Construtor";
 		if(sonarNumber < 0 ||sonarNumber > 8) {
 			throw std::invalid_argument("Numero invalido de sonar");
 		}
@@ -30,15 +33,20 @@ namespace sauron
 	{
 		SonarReading expectedReading, actualReading;
 		LineSegment matchedLineSegment;
+		SONAR_LOG(logDEBUG4) << "getEstimate (lastPose: " << last << ")";
 		if(m_model.validateReadings()) {
+			SONAR_LOG(logDEBUG3) << "Validou leituras (k = " << m_model.getReadingsBufferCount() << ")";
 			if(m_model.tryGetMatchingMapLine(m_localization.getMap(),
 				configs::sonars::validationGateSigma2,
 				&matchedLineSegment, &expectedReading, &actualReading)) {
+					SONAR_LOG(logDEBUG2) << "Pegou segmento: (" << matchedLineSegment.getEndPoint1().getX() << ", "<< matchedLineSegment.getEndPoint1().getY() << ") -> (" << matchedLineSegment.getEndPoint2().getX() << ", "<< matchedLineSegment.getEndPoint2().getY() << "))";
+					SONAR_LOG(logDEBUG2) << "Leitura esperada: " << expectedReading.getReading();
+					SONAR_LOG(logDEBUG2) << "Leitura recebida: " << actualReading.getReading();
 
                     z.resize( 1, 1 );
                     R.resize( 1, 1 );
                     H.resize( 1, 3 );
-                    hValue.resize( 1, 3 );
+                    hValue.resize( 1, 1 );
 
                     z.clear();
                     R.clear();
@@ -60,8 +68,14 @@ namespace sauron
 					pose_t x_sonar = sonarRelativePose.X();
 					pose_t y_sonar = sonarRelativePose.Y();
 					H(0,2) = x_sonar * ::sin(theta_diff) + y_sonar * ::cos(theta_diff);
+
+					SONAR_LOG(logDEBUG3) << "Matriz H: " << H;
 					return true;
+			} else {
+				SONAR_LOG(logDEBUG4) << "Não associou a segmento no mapa.";
 			}
+		} else {
+			SONAR_LOG(logDEBUG4) << "Não validou leituras.";
 		}
 		return false;
 	}
